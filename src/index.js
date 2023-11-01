8c6831fe8f2cb100884ecf87313ef09174d212e9
Juan Rassa <jmrassa.dev@gmail.com>
2023-11-01 21:59:59 +0000

    Integrate code + Add transition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-// const startButton = document.querySelector('.start-button');
+const body = document.querySelector('body');
+const welcomeScreen = document.querySelector('#welcome-screen');
+const gameScreen = document.querySelector('#game-screen');
+const startButton = document.querySelector('.start-button');
 
-document.querySelector('.start-button').addEventListener('click', function () {
+startButton.addEventListener('click', function () {
   console.log('it worked');
+  gameScreen.classList.remove('display-none');
+  gameScreen.classList.toggle('opacity-out');
+  welcomeScreen.classList.toggle('display-none');
+  body.classList.remove('body-welcome-state');
 });
